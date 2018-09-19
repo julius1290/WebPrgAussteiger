2cdd00ce4b998da0e9a867a4207a03fc87fcccb8
migster42 <miguel.wolf@migum.de>
2018-09-19 12:33:21 +0000

    Update aussteiger_js.js

--- public/js/aussteiger_js.js
+++ public/js/aussteiger_js.js
@@ -1,11 +1,75 @@
-$.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-});
+"use strict";
 
 const test = (a,b) => {
     alert (a+b);
 };
 
+$( document ).ready(function() {
+	$.ajaxSetup({
+	    headers: {
+	        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+	    }
+	});
+});
 
+
+/**
+ * Generieren einer Meldung
+ * @param  {String}  outputElement [description]
+ * @param  {String}  cssClass      [description]
+ * @param  {String}  text          [description]
+ * @param  {Boolean} append        [description]
+ */
+function createAlert(outputElement, cssClass, text, append = false) {
+    if (append === false) $(outputElement).empty();
+    let icon = null;
+    switch (cssClass) {
+        case "success": 
+          icon = '<i class="fa fa-check-circle"></i> '; 
+          break;
+        case "danger": 
+          icon = '<i class="fa fa-exclamation-circle"></i> '; 
+          break;
+        case "warning": 
+          icon = '<i class="fa fa-exclamation-triangle"></i> '; 
+          break;
+        case "info": 
+          icon = '<i class="fa fa-info-circle"></i> '; 
+          break;
+    }
+    $(outputElement).append('<div class="alert alert-' + cssClass + ' alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Schließen"><span aria-hidden="true">&times;</span></button>' + icon + ' ' + text + '</div>');
+}
+
+/**
+ * Validieren und Senden der Daten
+ */
+function sendData() {
+    let error = false;
+
+    let feld = $('#feld').val();
+    if (feld1 == '' || feld2 == '') error = true;
+
+    if (error === false) {
+        $.ajax({
+            type: "POST",
+            url: "ajax.php",
+            cache: false,
+            timeout: 5000,
+            data: {action: 'getData', feld1: feld},
+            success: function(data) {
+                if (data.status.toLowerCase() === "ok") {
+                	// TODO: Daten anzeigen
+                    // createAlert("#alert", "success", "Erfolgreich gespeichert!");
+                } else {
+                    createAlert("#alert", "warning", "Fehler beim Senden der Daten: " + data.message);
+                }
+            },
+            error: function() {
+                createAlert("#alert", "warning", "Fehler beim Senden der Daten: Fehler bei der Übertragung!");
+            }
+        });
+    }
+    else {
+        createAlert("#alert", "warning", "Bitte alle Felder ausfüllen!");
+    }
+}
