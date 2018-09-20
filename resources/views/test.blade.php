1452b4c2ede79c81d01afa67f73594810624a936
migster42 <miguel.wolf@migum.de>
2018-09-20 08:15:36 +0000

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,7 +1,99 @@
 @extends('layout')
 @section('content')
+<style>
+	.form-radio > input {
+    	display: none;
+	}
+	.form-radio > label {
+    display: inline-block;
+    height: 50px;
+    line-height: 1.22em;
+    margin: 2px;
+    padding: 6px 0;
+    text-align: center;
+    width: 50px;
+    border: 1px solid #0065a8;
+    color: #0065a8;
+    border-radius: 100px;
+}
+.form-radio > input:checked + label, .form-radio > input + label:hover {
+    color: #fff;
+    background-color: #0065a8;
+}
+	</style>
 <div style="padding-top: 15px">
     <div id="example-basic">
+        <div class="form-group">
+		<label>Geschlecht</label>
+		<fieldset class="form-radio">
+			<input id="maennlich" name="geschlecht" value="1" type="radio" required>
+			<label for="maennlich" title="Männlich"><i class="fas fa-male fa-2x"></i></label>
+			<input id="weiblich" name="geschlecht" value="2" type="radio" required>
+			<label for="weiblich" title="Weiblich"><i class="fas fa-female fa-2x"></i></label>
+		</fieldset>
+	</div>
+	<div class="form-group">
+		<label for="religion">Religion</label>
+		<div class="input-group">
+			<select class="form-control" id="religion" name="religion" required>
+				<option value="">Bitte wählen</option>
+				<option value="0">egal</option>
+				<option value="Christentum">Christentum</option>
+				<option value="Islam">Islam</option>
+				<option value="Buddhismus">Buddhismus</option>
+				<option value="Hindu">Hindu</option>
+				<option value="Atheist">Atheist</option>
+			</select>
+			<div class="input-group-append">
+				<span class="input-group-text"><i class="fas fa-cross"></i></span>
+			</div>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="regierungsform">Regierungsform</label>
+		<div class="input-group">
+			<select class="form-control" id="regierungsform" name="regierungsform" required>
+				<option value="">Bitte wählen</option>
+				<option value="0">egal</option>
+				<option value="Monarchie">Monarchie</option>
+				<option value="Demokratie">Demokratie</option>
+				<option value="Diktatur">Diktatur</option>
+				<option value="Kommunismus">Kommunismus</option>
+			</select>
+			<div class="input-group-append">
+				<span class="input-group-text"><i class="fas fa-landmark"></i></span>
+			</div>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="klima">Klima</label>
+		<div class="input-group">
+			<select class="form-control" id="klima" name="klima" required>
+				<option value="">Bitte wählen</option>
+				<option value="0">egal</option>
+				<option value="Warm">Warm</option>
+				<option value="Kalt">Kalt</option>
+				<option value="Gemäßigt">Gemäßigt</option>
+			</select>
+			<div class="input-group-append">
+				<span class="input-group-text"><i class="fas fa-sun"></i></span>
+			</div>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="gesundheit">Gesundheitsindex</label>
+		<div class="input-group">
+			<input type="range" class="custom-range" min="0" max="10" name="gesundheit" id="gesundheit">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="infrastruktur">Infrastrukturindex</label>
+		<div class="input-group">
+			<input type="range" class="custom-range" min="0" max="10" name="infrastruktur" id="infrastruktur">
+		</div>
+	</div>
+	<button type="reset" class="btn btn-danger">Abbrechen</button>
+	<button type="submit" class="btn btn-success">Suchen</button>
         <h3>Keyboard</h3>
         <section>
             <p>Try the keyboard navigation by clicking arrow left or right!</p>
