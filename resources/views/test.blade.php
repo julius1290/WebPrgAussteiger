1b6b4ef5d854839eb5db4c31fe28684090fcbb8a
migster42 <miguel.wolf@migum.de>
2018-09-20 08:45:48 +0000

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -34,6 +34,10 @@
                         <label for="maennlich" title="Männlich"><i class="fas fa-male fa-2x"></i></label>
                         <input id="weiblich" name="geschlecht" value="2" type="radio" required>
                         <label for="weiblich" title="Weiblich"><i class="fas fa-female fa-2x"></i></label>
+                        <input id="geschlechtslos" name="geschlecht" value="2" type="radio" required>
+                        <label for="geschlechtslos" title="Geschlechtslos"><i class="fas fa-genderless fa-2x"></i></label>
+                        <input id="transgender" name="geschlecht" value="2" type="radio" required>
+                        <label for="transgender" title="Transgender"><i class="fas fa-transgender fa-2x"></i></label>
                     </fieldset>
                 </div>
             </section>
