5a7bbd30416f928fee5aafa26c6b40a969890f70
Julius <privat@js-lights.de>
2018-09-27 11:01:58 +0200

    database request

--- public/js/aussteiger_js.js
+++ public/js/aussteiger_js.js
@@ -50,7 +50,7 @@ function createAlert(outputElement, cssClass, text, append = false) {
 function sendData() {
     let error = false;
 console.log('lel');
-    let geschlecht = $('#geschlecht').val();
+    let geschlecht = $("input[name='geschlecht']").val();;
     let religion = $('#religion').val();
     let regierungsform = $('#regierungsform').val();
     let klima = $('#klima').val();
@@ -64,7 +64,7 @@ console.log('lel');
             url: "/calculate",
             cache: false,
             timeout: 5000,
-            data: {geschlecht: geschlecht, regierungsform: regierungsform, klima: klima, gesundheit: gesundheit, infrastruktur: infrastruktur, religion: religion},
+            data:JSON.stringify({geschlecht: geschlecht, regierungsform: regierungsform, klima: klima, gesundheit: gesundheit, infrastruktur: infrastruktur, religion: religion}),
             success: function(data) {
                 alert("lol");
                 data = JSON.parse(data);
